51d9d2441fc0809a8921e4e479f6b04258178a6f
matthewrpacker <matthewrpacker@gmail.com>
2016-09-20 10:45:56 -0600

    Remove hashes from view - Test passes with hard coding store total count - Work on store total count next

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,9 +1,12 @@
 class Store
-  attr_reader :stores_summary
+  attr_reader :type, :name, :city, :distance, :phone
 
   def initialize(store_attributes)
-    byebug
-    @stores_summary = store_attributes
+    @type = store_attributes['storeType']
+    @name = store_attributes['longName']
+    @city = store_attributes['city']
+    @distance = store_attributes['distance']
+    @phone = store_attributes['phone']
   end
 
   def self.service
--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -3,6 +3,10 @@ class StoreService
     @response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,longName,city,distance,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_KEY']}")
   end
 
+  def stores_summary
+    parsed_response = JSON.parse(@response.body)
+    parsed_response['stores']
+  end
   #
   # def stores
   #   @parsed_response['stores']
