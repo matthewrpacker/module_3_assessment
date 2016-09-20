37890bb0b1645ad0a301adf9750aca1d1ebb6ea0
matthewrpacker <matthewrpacker@gmail.com>
2016-09-20 10:01:42 -0600

    Add remaining store count expectation to spec - Begin transitioning to service to remove hashes from view

---
+++ app/services/store_service.rb
@@ -0,0 +1,7 @@
+class StoreService
+  def initialize
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,longName,city,distance,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_KEY']}")
+    @parsed_response = JSON.parse(response.body)
+    @stores = @parsed_response['stores']
+  end
+end
--- spec/features/user_can_search_by_zip_spec.rb
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -10,10 +10,6 @@ describe 'User can search for stores' do
 
     expect(current_path).to eq('/search')
     expect(page).to have_content("17 Total Stores")
-
-    # And I should see stores within 25 miles of 80202
-    # And I should see a message that says "17 Total Stores"
-    # And I should see exactly 15 results
-    # And I should see the long name, city, distance, phone number and store type for each of the 15 results
+    expect(page).to have_selector(".store", count: 15)
   end
 end
