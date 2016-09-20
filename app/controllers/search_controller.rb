4c398ae750aea888595f2090d45fdbee873c41b2
matthewrpacker <matthewrpacker@gmail.com>
2016-09-20 09:55:42 -0600

    Pass search by zip spec - User can see total store count - User can see each stores required information - Next steps, add requirements to spec and remove hashes from view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,longName,city,distance,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_KEY']}")
-    parsed_response = JSON.parse(response.body)
-    @stores = parsed_response['stores']
+    @parsed_response = JSON.parse(response.body)
+    @stores = @parsed_response['stores']
   end
 end
